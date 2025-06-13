6b64c75bfcc4b8cb26c4b0bd8315dacf8a713283
jonayed-nasir <junayednasir073@gmail.com>
2025-06-13 17:24:05 +0600

    js practice v_1.1.7

--- Chapter3/TemplateStr.js
+++ Chapter3/TemplateStr.js
@@ -2,4 +2,20 @@
 const name = 'Jonayed';
 const age = 25;
 const message = `My name is ${name} and I am ${age} years old.`;
-console.log(message);
+// console.log(message);
+
+let firstName = 'Jonayed';
+let lastName = 'Nasir';
+let fullName = `My name is ${firstName} ${lastName}, and I am age ${18} years old`;
+// console.log(fullName);
+
+let sum = 5;
+let sum2 = 10;
+console.log(`The sum of (+) ${sum} and ${sum2} is ${sum + sum2}.`);
+console.log(`The sum of (-) ${sum} and ${sum2} is ${sum - sum2}.`);
+console.log(`The sum of (*) ${sum} and ${sum2} is ${sum * sum2}.`);
+console.log(`The sum of (/) ${sum} and ${sum2} is ${sum / sum2}.`);
+console.log(`The sum of (%) ${sum} and ${sum2} is ${sum % sum2}.`);
+console.log(`The sum of (**) ${sum} and ${sum2} is ${sum ** sum2}.`);
+console.log(`The sum of (++) ${sum} and ${sum2} is ${sum++ + sum2}.`);
+console.log(`The sum of (--) ${sum} and ${sum2} is ${++sum + sum2}.`);
