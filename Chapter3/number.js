1aa1bae0a94c97fed317baf3f44ba81b6d3cbc0a
jonayed-nasir <junayednasir073@gmail.com>
2025-06-15 22:53:48 +0600

    v_1.1.7

---
+++ Chapter3/number.js
@@ -0,0 +1,12 @@
+// !JavaScript Numbers
+
+let x = '54.445';
+let y = 563e4-43
+// console.log(x + y)
+// console.log(typeof x);
+// console.log(typeof parseInt(x));
+// console.log(parseInt(x));
+// console.log(parseFloat(x));
+
+let n = 843.5948;
+// console.log(n.toString()); // 843.59
