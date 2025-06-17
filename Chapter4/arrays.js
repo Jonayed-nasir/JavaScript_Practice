60c25a778a4be522829c29e0ec34268d548e56ce
jonayed-nasir <junayednasir073@gmail.com>
2025-06-17 14:28:40 +0600

    v_1.2.0

--- Chapter4/arrays.js
+++ Chapter4/arrays.js
@@ -45,3 +45,43 @@ const nameNum = ['jonayed', 75489, 'nasir', 'newPart'];
 // console.log(nameNum.join(' * '));
 // let myName = nameNum.join(', ');
 // console.log(myName)
+
+// ?The pop() method
+const array3 = ['apple', 'banana', 'orange', 'grape', 'kiwi', 43];
+// let lastElement = array3.pop()
+// array3.pop()
+// console.log(array3);
+array3.pop();
+
+// ?The push() method
+const array4 = ['apple', 'banana', 'orange', 'grape', 'kiwi', 43];
+// array4.push('75489');
+// console.log(array4);
+let lastElement = array4.push('haja');
+
+// ?The shift() method
+const array5 = ['apple', 'banana', 'orange', 'grape', 'kiwi', 43];
+array5.shift();
+let del = array5.shift();
+// console.log(array5);
+
+// ?The unShift() method
+const array6 = ['apple', 'banana', 'orange', 'grape', 'kiwi', 43];
+// array6.unshift('jonayed');
+let unshif = array6.unshift('mango');
+// console.log(array6);
+
+// ?The delete() method
+const array7 = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+
+delete array7[0];
+delete array7[1];
+// console.log(array7)
+
+//? The concat() method
+const array8 = [1, 2, 3, 4, 5, 6];
+const array9 = [7, 8, 9, 10];
+const array10 = [11, 12, 13, 14, 15];
+
+let joyen = array8.concat(array9, array10);
+// console.log(joyen);
