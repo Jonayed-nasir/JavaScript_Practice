68e3324b25444dbd94126a33f21ba535b3d500e4
jonayed-nasir <junayednasir073@gmail.com>
2025-06-19 20:50:27 +0600

    v_1.2.4

---
+++ Chapter4/math.js
@@ -0,0 +1,58 @@
+// !avaScript Math Object
+// The Math Object
+let pi = Math.PI;
+// console.log(pi);
+
+// ?the Math.round()
+
+// let round = Math.round(4.5);
+let round = Math.round(3.5);
+// console.log(round)
+// console.log(round)
+
+// ? the Math.ceil(4.1);
+
+// let ceil = Math.ceil(4.9);
+let ceil = Math.ceil(4.1);
+// console.log(ceil)
+// console.log(ceil);
+
+//? the Math.floor(4.9);
+// let floor = Math.floor(4.1);
+let floor = Math.floor(4.3);
+// console.log(floor);
+// console.log(floor);
+
+// ? the Math.trunc(4.7);
+// let trunc = Math.trunc(4.3);
+// let trunc = Math.trunc(4.7);
+let trunc = Math.trunc(-4.3);
+// console.log(trunc);
+
+//? the Math.sign();
+let sign = Math.sign('hello');
+// console.log(sign)
+
+//? the Math.pow()
+let pow = Math.pow(2, 5);
+// console.log(pow);
+
+// ? the Math.sqrt()
+let sqrt = Math.sqrt(64);
+// console.log(sqrt)
+
+//? the Math.min()
+let min = Math.min(1, 2, 3, -43);
+// console.log(min);
+
+//? the Math.max()
+let max = Math.max(1, 2, -3, 4, -4, 23);
+// console.log(max);
+
+//? the Math.random()
+let random = Math.random();
+// console.log(random);
+
+
+// ? the Math.abs()
+let abs = Math.abs(3.55)
