b0d45053538005cc49befa74be49302d6ae11a41
jonayed-nasir <junayednasir073@gmail.com>
2025-06-21 00:13:23 +0600

    v_1.2.6

---
+++ Chapter5/ifElse.js
@@ -0,0 +1,29 @@
+// !JavaScript if, else, and else if
+
+// ?if statement
+
+let age = 17;
+
+if (age >= 18) {
+  //   console.log('tumake kajer jnno parchec kraholo');
+}
+
+//? else statement
+
+if (age >= 18) {
+  // console.log('tumake kajer jnno parchec kraholo');
+} else {
+  //   console.log('tumi akhoni kajer jonno parfect na!');
+}
+
+//? else if statement
+
+let marks = 75;
+
+if (marks >= 80) {
+  //   console.log('You got A+');
+} else if (marks >= 70 && marks < 60) {
+  //   console.log('You got A');
+} else {
+  //   console.log('You got B');
+}
