d147b30889dfca34c9bb1dc02deb1fcf31030b0f
jonayed-nasir <junayednasir073@gmail.com>
2025-06-25 12:46:48 +0600

    v_1.2.10

---
+++ Chapter6/loops.js
@@ -0,0 +1,62 @@
+//! For Loops
+
+//! for loop er gthon
+// for (initializer, condition, increment){
+//
+//}
+//!
+
+//*
+// for (let i = 1; i <= 10; i++){
+// console.log(`${i} : Jonayed Nasir`)
+// }
+//*
+
+// for (let i = 0; i < 100; i += 3) {
+//     console.log(`${i} :'Jonayed nasir'`);
+// }
+
+// step number on not` common
+// for (let i = 2; i <= 100; i += 2) {
+//   console.log(i);
+// }
+
+// step number 2 common
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`${i} is Event Number`);
+//   }
+// }
+
+// odd number caowont
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 === 1) {
+//     console.log(`${i} is odd number`);
+//   }
+// }
+
+// sum number
+// let sum = 0;
+// for (let i = 0; i <= 5; i++) {
+//   console.log(sum + ' + ' + i + ' = ' + (sum + i));
+//   sum += i;
+// }
+
+// console.log('result = ' + sum);
+
+//!*
+
+// let sum = 1;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${sum} * ${i} = ${sum * i}`);
+//   sum *= i;
+// }
+
+// console.log(`result = ${sum}`);
+
+//? while Loop
+let i = 0;
+while (i < 10) {
+  console.log('hello jonayd nasir');
+  i++
+}
