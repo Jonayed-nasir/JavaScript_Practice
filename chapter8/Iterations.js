3702d6518aabaacef7cf708c7197e7dadf628f4a
jonayed-nasir <junayednasir073@gmail.com>
2025-07-05 22:33:05 +0600

    v_1.3.10

--- chapter8/Iterations.js
+++ chapter8/Iterations.js
@@ -6,8 +6,44 @@ const Info = ['jonayed', 'monir', 'shahed', 'sujon'];
 let text = '';
 Info.forEach(myFunction);
 function myFunction(value, number, array) {
-  text += value + '\n'
-//   text += number+'\n'
-//   text += array+'\n'
+  text += value + '\n';
+  //   text += number+'\n'
+  //   text += array+'\n'
 }
-console.log(text);
+// console.log(text);
+
+//? Javascritp Array map()
+
+let numbers = [1, 2, 3, 4, 5];
+let txt = '';
+let doubled = numbers.map(function (value, index, array) {
+  return value * 2;
+});
+
+// console.log(numbers);
+// console.log(doubled);
+
+let result = numbers.map((value) => value * 3);
+// console.log(result);
+
+//! 🧑‍💻 বাস্তব উদাহরণ
+
+let names = ['jonayed', 'nasir', 'tefheema'];
+
+let upperNames = names.map(function (name, index) {
+  return name.toUpperCase();
+});
+
+// console.log(upperNames);
+
+let numbers2 = [3, 6, 9];
+
+let output = numbers2.map(function (value) {
+  if (value % 2 == 0) {
+    return value + ' is even';
+  } else if (value % 2 == 1) {
+    return value + ' is odd ';
+  }
+});
+
+// console.log(output);
