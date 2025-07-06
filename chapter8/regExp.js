9f43b03febb37350879392e704663c10669cd5db
jonayed-nasir <junayednasir073@gmail.com>
2025-07-06 13:01:14 +0600

    v_1.3.1

---
+++ chapter8/regExp.js
@@ -0,0 +1,25 @@
+//? JavaScript Regular Expressions
+
+// let text = 'I Live JavaScript';
+// let position = text.search(/live/i);
+// console.log(position);
+
+// let text = 'I live JavaScript';
+// let newText = text.replace('JavaScript', 'Python');
+// console.log(newText);
+
+// let text = 'I love JavaScript and JavaScript is fun';
+// let result = text.replace(/JavaScript/g, 'Python');
+// console.log(result)
+
+// let text = 'My phone number is 01326277782'
+// let output = text.replace(/\d+/g, '[hidden]')
+// console.log(output)
+
+// let text = 'Call me at 01812345678';
+// let result = text.replace(/\d{11}/g, '[01**23**]');
+// console.log(result)
+
+// let email = 'user@gmail.com';
+// let result = email.replace(/\S+@\S+\.\S+/g, '[email hidden]');
+// console.log(result); // [email hidden]
